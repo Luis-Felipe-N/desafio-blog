c27cc584c48630a8c4524ee8529881a4aa0485e3
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-07-18 12:51:42 -0300

    'start'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import style from './header.module.scss'
+
 export default function Header() {
   return (
-    <header>
-      <Link href={"/"}>
-          <Image
-            alt="slsa"
-            src="/Logo.svg"
-            width="238.62"
-            height="25.63"
-          />
-      </Link>
-      sada
+    <header className={style.header}>
+      
+          sadsadasd
     </header>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+
+import { AiOutlineUser,AiOutlineCalendar } from 'react-icons/ai'
+import style from './home.module.scss';
 
 interface Post {
   uid?: string;
@@ -25,21 +27,60 @@ interface HomeProps {
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export default function Home() {
+export default function Home({posts}) {
+  console.log(posts)
   return (
-    <main>
-      <div>
-        <h1>Como utilizar Hooks</h1>
-        <p>Pensando em sincronização em vez de ciclos de vida.</p>
-        <time>15 Mar 2021</time> <span>Luis Felipe</span>
+    <main className={style.home}>
+      <div className={style.containerPosts}>
+        <div>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <time>
+            <AiOutlineCalendar />
+            15 Mar 2021
+          </time>
+          <span>
+            <AiOutlineUser />
+            Luis Felipe
+          </span>
+        </div>
+
+        <div>
+          <h1>Como utilizar Hooks</h1>
+          <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App.</p>
+          <time>
+            <AiOutlineCalendar />
+            15 Mar 2021
+          </time>
+          <span>
+            <AiOutlineUser />
+            Luis Felipe
+          </span>
+        </div>
+        <div>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <time>
+            <AiOutlineCalendar />
+            15 Mar 2021
+          </time>
+          <span>
+            <AiOutlineUser />
+            Luis Felipe
+          </span>
+        </div>
       </div>
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
-
-//   // TODO
-// };
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getAllByType('post');
+  
+  return {
+    props: {
+      posts: postsResponse
+    }
+  }
+};
